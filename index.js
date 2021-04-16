321588d66a9e50ef15eecf44c8f9486bb10084e0
Jahid101 <jahidhasananik.official@gmail.com>
2021-04-16 21:12:29 +0600

    some stuffs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
 
-const PORT = process.env.PORT || 7777;
+const PORT = process.env.PORT || 9999;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -26,6 +26,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const serviceCollection = client.db("fixmypc").collection("services");
   const feedbackCollection = client.db("fixmypc").collection("feedbacks");
+  const adminCollection = client.db("fixmypc").collection("admins");
+  const orderCollection = client.db("fixmypc").collection("orders");
 
 
   app.post('/addService', (req, res) => {
@@ -45,6 +47,15 @@ client.connect(err => {
   })
 
 
+  app.get('/serviceBooking/:id', (req, res) => {
+    const id = ObjectID(req.params.id)
+    serviceCollection.find({ _id: id })
+      .toArray((err, products) => {
+        res.send(products[0]);
+      })
+  })
+
+
   app.post('/addFeedback', (req, res) => {
     const newFeedback = req.body;
     feedbackCollection.insertOne(newFeedback)
@@ -52,5 +63,49 @@ client.connect(err => {
         res.send(result.insertedCount > 0)
       })
   })
+
+
+  app.get('/feedback', (req, res) => {
+    feedbackCollection.find()
+      .toArray((err, feedbacks) => {
+        res.send(feedbacks);
+      })
+  })
+
+
+  app.post('/addAdmin', (req, res) => {
+    const newAdmin = req.body;
+    adminCollection.insertOne(newAdmin)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+
+  app.get('/admin', (req, res) => {
+    adminCollection.find()
+      .toArray((err, admins) => {
+        res.send(admins);
+      })
+  })
+
+
+  app.post('/addOrder', (req, res) => {
+    const newOrder = req.body;
+    orderCollection.insertOne(newOrder)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+
+  app.delete('/deleteService/:id', (req, res) => {
+    const id = ObjectID(req.params.id)
+    serviceCollection.deleteOne({ _id: id })
+      .then(result => {
+        res.send(result.deletedCount > 0)
+      })
+  })
+
 
 });
